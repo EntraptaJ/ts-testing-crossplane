cf4e157c818a42b6913c6e619cba7980023cc02e
Entrapta Jones <august@mylogin.space>
2025-02-23 04:22:42 +0000

    feat: Slowly implement the Tinkerbell stuff

--- src/function.ts
+++ src/function.ts
@@ -1,19 +1,33 @@
-import { RunFunctionRequest, RunFunctionResponse } from "function-sdk-typescript/src/gen/v1/run_function_pb"
+import { RunFunctionRequest, RunFunctionResponse, Severity } from "function-sdk-typescript/src/gen/v1/run_function_pb"
 import * as logger from "function-sdk-typescript/src/logger"
 import { PartialMessage } from "@bufbuild/protobuf"
+import * as k8s from '@kubernetes/client-node'
+import * as test from '../tmp/test2'
 
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const k8sApi = kc.makeApiClient(k8s.CustomObjectsApi);
 const log = logger.getLogger();
 
 export async function runFunction(req: RunFunctionRequest): Promise<PartialMessage<RunFunctionResponse>> {
-    log.debug("Running function...");
+  log.debug('Running function...');
 
-    // TODO: Add your function logic here
 
-    log.debug("I was run!");
-    return {
-        conditions: [],
-        context: req.context,
-        desired: req.desired,
-        meta: req.meta
-    }
+
+  //const machineDetails = await k8sApi.getNamespacedCustomObject('infrastructure.cluster.x-k8s.io', 'v1beta1', 'core-prod', 'tinkerbellmachines', '')
+
+  log.debug('I was run!', req.input);
+  return {
+    conditions: [],
+    results: [
+      {
+        message: `Hello World! ${JSON.stringify(req)}`,
+        severity: Severity.NORMAL
+      }
+    ],
+    context: req.context,
+    desired: req.desired,
+    meta: req.meta
+  }
 }
